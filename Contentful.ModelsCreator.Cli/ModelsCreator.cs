783eed289df2f0a122c05c45a9e37db462f421f3
Robert Linde <robert@selberglinde.se>
2019-01-10 10:03:22 +0100

    Update package version

--- Contentful.ModelsCreator.Cli/ModelsCreator.cs
+++ Contentful.ModelsCreator.Cli/ModelsCreator.cs
@@ -39,7 +39,7 @@ namespace Contentful.ModelsCreator.Cli
         [Option(CommandOptionType.SingleValue, Description = "Path to the file or directory to create files in")]
         public string Path { get; }
 
-        [VersionOption("0.9.0")]
+        [VersionOption("0.9.1")]
         public bool Version { get; }
 
         private string _templateStart = @"using System;
